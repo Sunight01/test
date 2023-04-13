aec08271a6a202f25ff7a610c18f6be189a7cfae
Sunight <keviin.km01@gmail.com>
2023-04-12 23:51:18 -0400

    Se agrego un CIN

--- test.cpp
+++ test.cpp
@@ -2,8 +2,13 @@
 #include <string>
 using namespace std;
 
+
 int main(){
+    int ola = 0;
 
     cout << "Hello world!" << endl 
-    << "This is mi 1st time of GIT :) and this is a new change :))";
+    << "This is mi 1st time of GIT :) and this is a new change :))\n" << ">> ";
+    cin >> ola;
+    cin.ignore();
+    cout << ola << endl;
 }
