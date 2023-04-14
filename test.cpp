540c4a14d8449c4e71952aaeec993114a5e76dd9
Sunight <keviin.km01@gmail.com>
2023-04-13 22:47:36 -0400

    name added

--- test.cpp
+++ test.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
+    string name;
 
     cout << "Hello world!" << endl 
-    << "This is mi 1st time of GIT :) and this is a new change :))";
+    << "This is mi 1st time of GIT :) and this is a new change :))" << endl
+    << "como te llamas?" << "\n>> ";
+    cin >> name;
+    cout << "\nHola " << name << "!" << " un gusto en conocerte ^^";
 }
