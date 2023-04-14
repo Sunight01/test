606f988ac319b753171f6816186df3f88607a9f3
Sunight <keviin.km01@gmail.com>
2023-04-13 22:42:22 -0400

    nueva edicion de la rama

--- test.cpp
+++ test.cpp
@@ -10,5 +10,5 @@ int main(){
     << "This is mi 1st time of GIT :) and this is a new change :))\n" << ">> ";
     cin >> ola;
     cin.ignore();
-    cout << ola << endl;
+    cout << "El numero que colocaste es: " << ola;
 }
