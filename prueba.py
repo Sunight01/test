5f95318db37bb3b591257956c5b60dcb5341b0c9
Sunight <keviin.km01@gmail.com>
2023-04-25 22:19:37 -0400

    mini arreglo

--- prueba.py
+++ prueba.py
@@ -1,8 +1,10 @@
 def Hola():
     i = 0
-    i = eval(input("Escribe un numero desde el 1: "))
+    i = eval(input("Escribe un numero (desde el 1): "))
     if(i == 1):
         print("This is my world!")
+    elif(i == 2):
+        print("You should know this place")
     else:
         print("This is another world!")
     return 0
